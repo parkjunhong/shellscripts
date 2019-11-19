6ec58ba8c43aea84506d38fc88cc5801ffc40b71
Park Jun-Hong <parkjunhong77@gmail.com>
2019-11-19 14:04:00 +0900

    Create dynamic_var_binding.sh

---
+++ bash/lang/dynamic_var_binding.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# dynamic variable binding
+var1=""
+
+# $1 {string} variable name
+# $2 {any} value
+assign(){
+    _v=\$$1
+    echo "assign()._v=$_v"
+    _val=$(eval echo $_v)
+    if [ -z "$_val" ];
+    then
+        echo "not assigned"
+    else
+        echo "$_v.current=$_val"
+    fi
+
+    echo "eval $1=\"$2\""
+    eval $1=\"$2\"
+}
+
+echo
+assign var1 hi
+echo "var1=$var1"
+
+echo
+assign var1 "nice to meet u"
+echo "var1=$var1"
+
+echo
+assign var1 "me, too"
+echo "var1=$var1"
