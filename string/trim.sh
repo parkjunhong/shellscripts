77836fffd953d8e5af73c6d961c19d16d18d08e9
Park Jun-Hong <parkjunhong77@gmail.com>
2020-01-14 18:59:25 +0900

    Create trim.sh

---
+++ string/trim.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# $1 {string}: a string to be trimmed.
+# $return 'echo'
+trim(){
+  if [ $# -lt 1 ];
+  then
+    echo
+    return 0
+  fi
+  
+  str=$1
+  # trim from beginning
+  str=${str##*( )}
+  # trim from end
+  str=${str%%*( )}
+  
+  echo ${str}
+}
+
+STR=" HI! "
+echo ">${STR}<"
+STR=$(trim "${STR}")
+echo ">${STR}<"
+
+exit 0
