3c031239a1257be0ba12a96b6d8353d9c3257b81
Park Jun-Hong <parkjunhong77@gmail.com>
2020-01-14 19:46:26 +0900

    Update trim.sh

--- string/trim.sh
+++ string/trim.sh
@@ -4,19 +4,25 @@
 # $1 {string}: a string to be trimmed.
 # $return 'echo'
 trim(){
-  if [ $# -lt 1 ];
-  then
-    echo
-    return 0
-  fi
+	if [ $# -lt 1 ];
+	then
+		echo
+		return 0
+	fi
   
-  local str=$1
-  # trim from beginning
-  str=${str##*( )}
-  # trim from end
-  str=${str%%*( )}
-  
-  echo ${str}
+	local str=$1
+	# trim from beginning
+	while [[ $str == ' '* ];
+	do
+		str=${str## }"
+	done
+	# trim from end
+	while [[ $str == *' ' ];
+	do
+		str=${str%% }"
+	done
+	
+	echo "${str}"
 }
 
 STR=" HI! "
