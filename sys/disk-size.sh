d612869641a65bf2b0317f35d4956ce23140c365
Park Jun-Hong <parkjunhong77@gmail.com>
2020-02-17 17:35:44 +0900

    Update disk-size.sh

--- sys/disk-size.sh
+++ sys/disk-size.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# @author: parkjunhong77_gmail_com
-# @license: MIT.
-#
-
 help(){
 	if [ ! -z "$1" ];
 	then
@@ -13,14 +9,18 @@ help(){
 	
 	echo
 	echo "Usage:"
-	echo "disk-size <directory> -r"
+	echo "disk-size <directory> -s[a|d]r"
 	echo
 	echo "[Optons]"
+	echo " -s: sort."
+	echo "    + a: asc"
+	echo "    + d: desc"
 	echo " -r: Enable recursive search."
 }
 
 DIR=""
 RECUR=0
+SORT="N"
 while [ "$1" != "" ];
 do
 	case $1 in
@@ -37,8 +37,26 @@ do
 			while [ $idx -lt $len ];
 			do
 				case ${opt:${idx}:1} in 
+					h|-help)
+						help
+						exit 0
+						;;
 					r)
 						RECUR=1
+						;;
+					s)
+						case ${opt:((${idx}+1)):1} in 
+							a)
+								SORT="A"
+								((idx++))
+								;;
+							d)
+								SORT="D"
+								((idx++))
+								;;
+							*)
+								;;
+						esac
 						;;
 					*)
 						;;
@@ -77,45 +95,106 @@ abspath(){
 
 # @param $1 {string} directory
 deltailslash(){
-  if [ "$1" == "/" ];
-  then
-    echo $1
-  elif [ ! -z "$1" ] && [[ "$1" == */ ]]; 
-  then
-    echo ${1:0:$((${#1}-1))}
-  else
-    echo $1
-  fi  
+	if [ "$1" == "/" ];
+	then
+		echo $1
+	elif [ ! -z "$1" ] && [[ "$1" == */ ]]; 
+	then
+		echo ${1:0:$((${#1}-1))}
+	else
+		echo $1
+	fi	
 }
 
-# @param $1 list 
+#
+# @param $1 {string} du result
+# @return 'echo'
+tonum(){
+	local str=$(echo $1 | tr [:lower:] [:upper:])
+	local val=${str:0:((${#str}-1))}
+	local unit=${str:((${#str}-1)):1}
+	
+	# grater than or equal to a kilo
+	case ${unit} in
+		K)
+			echo ${val} | awk '{print $1 * 1024}'
+			;;
+		M)
+			echo ${val} | awk '{print $1 * 1024 * 1024}'
+			;;
+		G)
+			echo ${val} | awk '{print $1 * 1024 * 1024 * 1024}'
+			;;
+		T)
+			echo ${val} | awk '{print $1 * 1024 * 1024 * 1024 * 1024}'
+			;;
+		P)
+			echo ${val} | awk '{print $1 * 1024 * 1024 * 1024 * 1024 * 1024}'
+			;;
+		*)
+			echo ${str}
+			;;
+	esac
+}
+# 
+# @param $1 {string} parent directory 
+# @param $2 ~ 
 search(){
-    local args=($@)
-    local parent=${args[0]}
-    local subfiles=${args[@]:1}
-    local path=""
-    RST_FORMAT="[%s] %6s %s\n"
+	local args=($@)
+	local parent=${args[0]}
+	local subfiles=${args[@]:1}
+	local path=""
+	RST_FORMAT="[%s] %6s %s\n"
 
-    if [ "${parent}" == "/" ];
-    then
-        parent=""
-    fi
+	if [ "${parent}" == "/" ];
+	then
+		parent=""
+	fi	
+	
+	local __tmpfile__=$(pwd)"/.disk-size-tmp-$(date +%s)"
+	printf "%s" "" > ${__tmpfile__}
 
-    for file in ${subfiles[@]}
-    do
-        path=${parent}/${file}
-        IFS=" " read -a durst <<< $(du -sh ${path})
-        if [ -d "${path}" ];
-        then
-            printf "$RST_FORMAT" "d" ${durst[0]} ${durst[1]}
-        elif [ -f "${path}" ];
-        then
-            printf "$RST_FORMAT" "f" ${durst[0]} ${durst[1]}
-        fi
-    done
+	for file in ${subfiles[@]}
+	do	
+		path=${parent}/${file}
+		IFS=" " read -a durst <<< $(du -sh ${path})
+		printf "%030.1f=%s	%s\n" $(tonum ${durst[0]}) ${durst[0]} ${durst[1]} >> ${__tmpfile__}
+	done
+	
+	local __cmd__=""
+	case ${SORT} in
+		A)
+			__cmd__="cat ${__tmpfile__} | sort"
+			;;
+		D)
+			__cmd__="cat ${__tmpfile__} | sort -r"
+			;;
+		N)
+			__cmd__="cat ${__tmpfile__}"
+			;;
+		*)
+			help "Invalid Sort Type. value=${SORT}"
+			exit 1
+			;;
+	esac
+
+	local path=""
+	while IFS="=	" read -a durst
+	do
+		#echo "read: ${durst[@]}"
+		path="${durst[2]}"
+		if [ -f "${path}" ];
+		then
+			printf "$RST_FORMAT" "f" ${durst[1]} ${durst[2]}
+		elif [ -d "${path}" ];
+		then
+			printf "$RST_FORMAT" "d" ${durst[1]} ${durst[2]}
+		fi
+	done <<< "$(eval ${__cmd__})"
+	# "$(cat ${__tmpfile__} | sort)"
+
+	rm -f ${__tmpfile__}
 }
-
-
 
 DIR=$(deltailslash ${DIR})
 SUB_FILES=($(ls ${DIR}))
