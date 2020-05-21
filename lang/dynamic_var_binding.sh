e9d91f4e2db510d5a633831582acd54239c59e16
Park Jun-Hong <parkjunhong77@gmail.com>
2020-05-21 15:00:18 +0900

    Update dynamic_var_binding.sh

--- lang/dynamic_var_binding.sh
+++ lang/dynamic_var_binding.sh
@@ -20,6 +20,33 @@ assign(){
     eval $1=\"$2\"
 }
 
+## check & enable cmd-options
+# @param $1 {string}: a variable name of 'CMD-Option'
+# @param $2 {any}   : a expected current value'
+# @param $3 {any}   : a value if checking is passed.
+# @param $4 {string}: error message
+enable-cmd-option(){
+  _v=\$$1
+  _val=$(eval echo $_v)
+  if [ -z "$_val" ];
+  then
+    echo
+    echo " * * * '$_v' does not exist or is not assigned."
+    eval $1=\"$2\"
+    _val=$(eval echo $_v)
+  fi  
+
+  if [ "$_val" == "$2" ];
+  then
+    eval $1=\"$3\"
+  else
+    echo
+    echo " * * * '$_v' has a unexpected value. value=$_val"
+    help "$4"
+    exit 1
+  fi  
+}
+
 echo
 assign var1 hi
 echo "var1=$var1"
