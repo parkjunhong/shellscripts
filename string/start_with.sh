08216d35ee56b350a04cba58d74c233421dbe0d4
Park Jun-Hong <parkjunhong77@gmail.com>
2023-04-21 20:50:44 +0900

    Update start_with.sh
    bug fixed. 
    
    - z(.)*  is wrong grammar

--- string/start_with.sh
+++ string/start_with.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 'starts with'
-[[ $STRING == z(.)* ]]   # True if $STRING starts with an "z" (pattern matching).
+[[ $STRING == z* ]]   # True if $STRING starts with an "z" (pattern matching).
 [[ $STRING == "z*" ]] # True if $STRING is equal to z* (literal matching).
 
 [ $STRING == z* ]     # File globbing and word splitting take place.
