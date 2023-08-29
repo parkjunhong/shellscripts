1e5028c3aa1c651e1aabffc5548914b785108824
parkjunhong <parkjunhoong77@gmail.com>
2023-08-29 14:55:55 +0900

    사용법 추가

--- utils/tar-zcf-deploy-profile.sh
+++ utils/tar-zcf-deploy-profile.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+help(){
+	echo
+	echo "[Usage]"
+	echo " tar-zcf-deploy-profi.esh <profile> <build-name>"
+	echo 
+	echo "[Parameters]"
+	echo "- profile: profile에 해당하는 폴더명"
+	echo "- build-name: 압축파일명. 입력하지 않은 경우 상위 디렉토리명을 사용"
+	echo
+}
+
 function remove_left_zero() {
 	echo $1|sed -e "s/^0*//"
 }
@@ -9,6 +20,7 @@ cur_dir=$(pwd)
 if [[ $cur_dir != */deploy ]];then
 	echo
 	echo "Illegal directory. MUST BE end with '*/deploy'."
+	help
 	exit 0
 fi
 
@@ -18,6 +30,7 @@ profile=$(echo ${profile/\//})
 if [ ! -d "$cur_dir/$profile" ];then
 	echo
 	echo "존재하지 않는 프로파일 입니다. 입력=$profile"
+	help
 	exit 0
 fi
 
@@ -25,10 +38,15 @@ paths=($(echo $cur_dir | tr "/" " "))
 if [ ${#paths[@]} -lt 2 ];then
 	echo
 	echo "Illegal directory. The length of directory MUST BE greater than 1."
+	help
 	exit 1
 fi
 
-build_name=${paths[-2]}
+if [ -z "$2" ];then
+	build_name=${paths[-2]}
+else
+	build_name="$2"
+fi
 cur_date=$(date +%Y%m%d)
 filename=$build_name"-"$profile"-"$cur_date
 
