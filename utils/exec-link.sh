409b3e74787ad5a1f237846641c5c57e32251b1d
parkjunhong <parkjunhoong77@gmail.com>
2023-09-01 16:09:49 +0900

    코드 개선

--- utils/exec-link.sh
+++ utils/exec-link.sh
@@ -6,13 +6,21 @@
 # @license: MIT
 # =======================================
 
-
+assert-empty(){
+	if [ -z "$1" ];then
+		echo "'$2' does not exist."
+		exit 0
+	fi
+}
 
 LN_ABS_PATH=$(command -v $1)
-EXEC_DIR=$(dirname $(readlink -f $LN_ABS_PATH))
+assert-empty "$LN_ABS_PATH" "$1"
+LINK_PATH=$(readlink -f $LN_ABS_PATH)
+assert-empty "$LINK_PATH" "$LN_ABS_PATH"
+EXEC_DIR=$(dirname $LINK_PATH)
 cd "$EXEC_DIR"
 
-CMD="$LN_ABS_PATH"
+CMD="$LINK_PATH"
 shift 
 while [ ! -z "$1" ];do
 	CMD="$CMD ""$1"""
