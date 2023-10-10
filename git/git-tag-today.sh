91a1062c40d56b9203a20fa1fbac7e91cb8d6e36
parkjunhong <parkjunhong77@gmail.com>
2023-10-10 17:56:24 +0900

    프로젝트 및 branch 검증 완료.
    - TODO
     + tag 생성 및 push

--- git/git-tag-today.sh
+++ git/git-tag-today.sh
@@ -6,35 +6,95 @@
 # - tag 메시지 설정
 
 FILENAME=$(basename -- $0)
-TGT_DIR="."
-
 at(){
 	echo "($FILENAME:$1)"
 }
 
-if [ ! -z "$1" ] && [ ! -d "$1" ];then
-	echo "! ! ! '$1' is not a valid directory. $(at $LINENO)"
+
+help(){
+    if [ $# -gt 0 ];
+    then
+        echo
+        echo "['${FUNCNAME[1]}' says] " $1
+    fi
+    echo
+    echo "[Usage]"
+    echo "git-tag-today.sh <directory> -b <branch> [-m <tag message>] [-t <tag type>] [ --prefix <tag prefix>] [--suffix <tag suffix>] "
+    echo
+    echo "[Parameters]"
+    echo " directory: directory that contains git projects."
+    echo
+	echo "[Options]"
+	echo " -b | --branch : branchname"
+	echo " -m | --message: tag message"
+	echo " -t | --type   : tag type"
+	echo " --prefix      : tag name prefix"
+	echo " --suffix      : tag name suffix"
+}
+
+DIR="."
+BRANCH=""
+MSG=""
+TYPE="release"
+PREFIX=""
+SUFFIX=""
+while [ ! -z "$1" ];
+do
+	case "$1" in
+		-b | --branch)
+			shift
+			BRANCH="$1"
+			;;
+		-m | --message)
+			shift
+			MSG="$1"
+			;;
+		-t | --type)
+			shift
+			TYPE="$1"
+			;;
+		--prefix)
+			shift
+			PREFIX="$1"
+			;;
+		--suffix)
+			shift
+			SUFFIX="$1"
+			;;
+		*)
+			if [ ! -d "$1" ];then
+				echo "! ! ! [INVALID] '$1' is not a directory. $(at $LINENO)" 
+				exit 1
+			fi
+			DIR="$1"
+			;;
+	esac
+	shift
+done
+
+if [ -z "$BRANCH" ];then
+	echo "! ! ! [INVALID] 'branch' MUST BE assigned. $(at $LINENO)"
 	exit 1
 fi
 
-if [ ! -z "$1" ];then
-	TGT_DIR="$1"
+if [ "$DIR" != "." ];then
+	cd $DIR
 fi
 
-if [ "$TGT_DIR" != "." ];then
-	cd $TGT_DIR
-fi
-LEN_TGT=${#TGT_DIR}
-CMD_DIR="find $TGT_DIR -maxdepth 1 -type d | sort "
+# absolute path of directory 
+DIR=$(pwd)
+LEN_DIR=${#DIR}
+CMD_DIR="find $DIR -maxdepth 1 -type d | sort "
+# sub directories as projects
 PRJS=$(eval $CMD_DIR)
 
 for _prj in ${PRJS[@]};
 do
-	if [ "$_prj" == "$TGT_DIR" ];then
+	if [ "$_prj" == "$DIR" ];then
 		continue
 	fi
 	# target directory 로 시작하는 경로정보를 삭제
-	_prj=${_prj:$LEN_TGT}
+	_prj=${_prj:$LEN_DIR}
 
 	# /xxx 패턴
 	if [[ "$_prj" =~ /* ]];then
@@ -44,20 +104,33 @@ do
 	# go to 'project' directory
 	cd "$_prj"
 	# check 'git configuration' directory
-	if [ ! -d ".git" ];then
-		echo "! ! ! [NOT GIT PROJEcT] project=$_prj $(at $LINENO)"
-	else
-		_branch="devx"
-		if [ ! -z "$(git rev-parse --verify $_branch 2>/dev/null)" ];then
-			echo "'$_prj:$_branch' exists"
-		elif [ ! -z "$(git rev-parse --verify remote/origin/$_branch 2>/dev/null)" ];then
-			echo "pull 'remote/origin/$_branch'"
-			echo "'$_prj:remote/origin/$_branch' exists"
+	if [ -d ".git" ];then
+		_cur_branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+		_exist=0
+		if [ ! -z "$(git rev-parse --verify $BRANCH 2>/dev/null)" ] || [ ! -z "$(git rev-parse --verify remotes/origin/$BRANCH 2>/dev/null)" ];then
+			_exist=1
 		else
-			echo "! ! ! [NOT EXIST] '$_prj:$_branch' $(at $LINENO)"
+			echo "! ! ! [NOT EXIST] $_prj:$BRANCH $(at $LINENO)"
 		fi
+
+		if [ $_exist -eq 1 ];then
+			_modified=0
+			if [ "$_cur_branch" != "$BRANCH" ];then
+				git checkout $BRANCH 1>/dev/null
+				_modified=1
+			fi
+			echo "* * * [EXIST] $_prj -> $BRANCH"
+			echo "git tag -a <tag name> -m <tag message>"
+			echo "git push origin <tag name>"
+
+			if [ $_modified -eq 1 ];then
+				git checkout $_cur_branch 1>/dev/null
+			fi
+		fi
+	#else
+	#	echo "! ! ! [NOT GIT PRoJeCT] project=$_prj $(at $LINENO)"
 	fi
-	
+	# go to upper directory
 	cd ..
 done
 
