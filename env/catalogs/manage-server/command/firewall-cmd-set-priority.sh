973d31b1367a61ecb6a7199296907861d55a0a3a
Park Jun-Hong <parkjunhong77@gmail.com>
2026-06-17 14:02:39 +0900

    Update firewall-cmd-set-priority.sh

--- env/catalogs/manage-server/command/firewall-cmd-set-priority.sh
+++ env/catalogs/manage-server/command/firewall-cmd-set-priority.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
-sudo firewall-cmd --permanent --zone=trusted --set-priority=-10
-sudo firewall-cmd --permanent --zone=work --set-priority=-7
-sudo firewall-cmd --permanent --zone=internal --set-priority=-4
-sudo firewall-cmd --permanent --zone=external --set-priority=-1
-sudo firewall-cmd --reload
+run_cmd() {
+    local -a cmd=("$@")
+
+    echo
+    echo "${cmd[*]}"
+    "${cmd[@]}"
+}
+
+run_cmd sudo firewall-cmd --permanent --zone=trusted  --set-priority=-10
+run_cmd sudo firewall-cmd --permanent --zone=work     --set-priority=-7
+run_cmd sudo firewall-cmd --permanent --zone=internal --set-priority=-4
+run_cmd sudo firewall-cmd --permanent --zone=external --set-priority=-1
+run_cmd sudo firewall-cmd --permanent --zone=external --set-priority=-1
+
+run_cmd sudo firewall-cmd --reload
 
 exit 0
